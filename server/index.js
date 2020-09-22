1453edf5aee99e935c4ebf0c20f95c8d7ba28361
1000copy <1000copy@gmail.com>
2020-09-22 15:22:03 +0800

    server/index

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const express = require("express");
+const app = express();
+const { ApolloServer,PubSub } = require("apollo-server-express");
+const pubsub = new PubSub();
+const channelName = 'message'
+var resolvers = {
+  Mutation: {
+    addMessage: (root, { input }) => {
+      pubsub.publish(channelName, { messageAdded: input })
+      return input
+    },
+  },
+  Subscription: {
+    messageAdded: {
+      subscribe: (parent, args, {  }) => pubsub.asyncIterator(channelName),
+    },
+  },
+}
+const server = new ApolloServer({
+  typeDefs: `
+    type Query{
+      _empty:String 
+    }
+    type Mutation {
+      addMessage (input: String!): String!  
+    }
+    type Subscription {
+      messageAdded: String!
+    }
+  `,
+  resolvers,  
+});
+server.applyMiddleware({ app});
+const httpServer = http.createServer(app);var port = 4000;
+httpServer.listen({ port }, () => {
+  server.installSubscriptionHandlers(httpServer);
+  console.log(`graphql ready at http://localhost:${port}/graphql`);
+  console.log(`🚀 Subscriptions ready at ws://localhost:${port}${server.subscriptionsPath}`)
+});
